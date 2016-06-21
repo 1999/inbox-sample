38a14b22406ecdb891e90a635a7107e10b036325
Dmitry Sorin <dsorin@yandex-team.ru>
2016-06-21 08:23:52 +0300

    Add header component stub

--- components/inbox-sample-header/index.js
+++ components/inbox-sample-header/index.js
@@ -1,33 +1,35 @@
 'use strict';
 
+import store from '../../store';
+import style from './style.scss';
+
 const CUSTOM_TAG_NAME = 'inbox-sample-header';
-const CUSTOM_TAG_TEMPLATE = `
-    <template id="inbox-sample-root">
-        <header>header</header>
-        <nav>nav</nav>
-        <div>content</div>
-    </template>`
 
 class InboxSampleHeader extends HTMLElement {
 
     createdCallback() {
-        // document.body.insertAdjacentHTML(CUSTOM_TAG_TEMPLATE, 'beforeend');
+        this._onStoreChange = this._onStoreChange.bind(this);
+
+        // pre-build HTML
+        const state = store.getState();
+        const template = document.querySelector(`#${CUSTOM_TAG_NAME}`);
+        const cloneFragment = document.importNode(template.content, true);
+
+        this.appendChild(cloneFragment);
+    }
+
+    attachedCallback() {
+        this._unStoreChange = store.subscribe(this._onStoreChange);
     }
 
     detachedCallback() {
-        // const template = document.querySelector(`#${CUSTOM_TAG_NAME}`);
-        // const clone = document.importNode(template.content, true);
+        this._unStoreChange();
+    }
 
-        // document.body.appendChild(clone);
+    _onStoreChange() {
+        const state = store.getState();
     }
 
 }
 
 document.registerElement(CUSTOM_TAG_NAME, InboxSampleHeader);
-
-export default function (store) {
-    const elem = document.createElement(CUSTOM_TAG_NAME);
-    elem.store = store;
-
-    return elem;
-};

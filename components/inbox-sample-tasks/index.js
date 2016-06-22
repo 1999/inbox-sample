daf209a52094229c1073a11fe8409940aa0cb6d0
Dmitry Sorin <dsorin@yandex-team.ru>
2016-06-22 08:39:58 +0300

    Use flexbox for positioning, add titles for icons

--- components/inbox-sample-tasks/index.js
+++ components/inbox-sample-tasks/index.js
@@ -1,30 +1,44 @@
 'use strict';
 
+import store from '../../store';
+import style from './style.scss';
+
 const CUSTOM_TAG_NAME = 'inbox-sample-tasks';
-const CUSTOM_TAG_TEMPLATE = `
-    <template id="inbox-sample-root">
-        <header>header</header>
-        <nav>nav</nav>
-        <div>content</div>
-    </template>`
 
 class InboxSampleTasks extends HTMLElement {
 
     createdCallback() {
-        // document.body.insertAdjacentHTML(CUSTOM_TAG_TEMPLATE, 'beforeend');
+        this._onStoreChange = this._onStoreChange.bind(this);
+
+        // // pre-build HTML
+        // const state = store.getState();
+        // const templateNavGroup = document.querySelector(`#${CUSTOM_TAG_NAME}`);
+
+        // this._updateVisibility();
+
+        // state.menu.groups.forEach((group, index) => {
+        //     const cloneFragment = document.importNode(templateNavGroup.content, true);
+        //     const navGroup = cloneFragment.querySelector('inbox-sample-navgroup');
+
+        //     navGroup.id = group.id;
+        //     navGroup.last = (index + 1 === state.menu.groups.length);
+
+        //     this.appendChild(cloneFragment);
+        // });
+    }
+
+    attachedCallback() {
+        this._unStoreChange = store.subscribe(this._onStoreChange);
     }
 
     detachedCallback() {
         this._unStoreChange();
     }
 
+    _onStoreChange() {
+        // this._updateVisibility();
+    }
+
 }
 
 document.registerElement(CUSTOM_TAG_NAME, InboxSampleTasks);
-
-export default function (store) {
-    const elem = document.createElement(CUSTOM_TAG_NAME);
-    elem.store = store;
-
-    return elem;
-};

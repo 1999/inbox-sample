74ead5a0701782d5bf70f20e3768d25a10471a1a
Dmitry Sorin <dsorin@yandex-team.ru>
2016-06-27 06:59:06 +0300

    Fix some UI issues: transitionend for nav/tasks, manage div for task

--- components/inbox-sample-tasks/index.js
+++ components/inbox-sample-tasks/index.js
@@ -10,15 +10,9 @@ class InboxSampleTasks extends HTMLElement {
 
     createdCallback() {
         this._onStoreChange = this._onStoreChange.bind(this);
+        this._onTransitionEnd = this._onTransitionEnd.bind(this);
 
         const state = store.getState();
-        const periods = new Set([
-            {from: 'today start', to: 'today end'},
-            {from: 'yesterda start', to: 'yesterda end'},
-            {from: 'current month start', to: 'yesterdat start'},
-            {from: '1970', to: 'current month start'}
-        ]);
-
         const threadDays = state.tasks.reduce((memo, task) => {
             const date = new Date(task.lastDate);
             const dateStr = `${date.getFullYear()}.${date.getMonth()}.${date.getDate()}`;
@@ -41,6 +35,10 @@ class InboxSampleTasks extends HTMLElement {
 
     attachedCallback() {
         this._unStoreChange = store.subscribe(this._onStoreChange);
+
+        const state = store.getState();
+        this._showMenuSetting = state.menu.show;
+        this._navElem = document.querySelector('inbox-sample-nav');
     }
 
     detachedCallback() {
@@ -48,7 +46,30 @@ class InboxSampleTasks extends HTMLElement {
     }
 
     _onStoreChange() {
-        // this._updateVisibility();
+        const state = store.getState();
+        const newMenuSetting = state.menu.show;
+
+        if (newMenuSetting !== this._showMenuSetting) {
+            this._showMenuSetting = newMenuSetting;
+
+            if (this._showMenuSetting) {
+                this._changeWidth();
+            } else {
+                this._navElem.addEventListener('transitionend', this._onTransitionEnd, false);
+            }
+        }
+    }
+
+    _onTransitionEnd({propertyName}) {
+        if (propertyName === 'transform') {
+            this._changeWidth();
+            this._navElem.removeEventListener('transitionend', this._onTransitionEnd);
+        }
+    }
+
+    _changeWidth() {
+        this.classList.toggle('wide', !this._showMenuSetting);
+
     }
 
 }
